4ab1c39e3d3609273045dc0db8e60a42c03bc405
chirkov <d@chirkov.org>
2015-03-09 16:13:09 +0500

    Touch catapulte

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -13,21 +13,65 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
+    CCNode *_mouseJointNode;
+    CCPhysicsJoint *_mouseJoint;
 }
 
-- (void)didLoadFromCCB {
-    
+- (void)didLoadFromCCB
+{
     self.userInteractionEnabled = true;
+    
+    _physicsNode.debugDraw = true;
+    
+    _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
 }
 
-- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
-    [self launchPenguin];
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
+    {
+        _mouseJointNode.position = touchLocation;
+        
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+    }
 }
 
-- (void)launchPenguin {
+- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
+- (void)releaseCatapult {
+    if (_mouseJoint != nil)
+    {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
+    }
+}
+
+-(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // when touches end, meaning the user releases their finger, release the catapult
+    [self releaseCatapult];
+}
+
+-(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+    [self releaseCatapult];
+}
+
+- (void)launchPenguin
+{
     CCNode* penguin = [CCBReader load:@"Penguin"];
     penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
     [_physicsNode addChild:penguin];
